adee5e6dce22f9fc8fdaf7eee4eca167b44b9b43
tylerchessa <tylerchessa@gmail.com>
2023-03-21 21:21:31 -0700

    interviewer list

---
+++ src/components/DayList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import DayListItem from "./DayListItem";
+
+function DayList(props) {
+  // if (!props.day) {
+  //   return [];
+  // }
+  const dayListArray = props.days ? props.days.map((day)=> {
+    return <DayListItem 
+        key={day.id}
+        name={day.name} 
+        spots={day.spots} 
+        selected={day.name === props.day}
+        setDay={props.setDay}  
+      />
+  }) : [];
+  return <ul>{dayListArray}</ul>
+}
+
+export default DayList;
---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import "components/InterviewerListItem.scss"
+import classNames from "classnames";
+
+function InterviewerListItem(props) {
+const InterviewerClass = classNames('interviewers__item', {'interviewers__item--selected': props.selected === true})
+
+function interviewerName() {
+  return (props.selected ? props.name : "");
+}
+
+  return (
+    <li onClick={() => props.setInterviewer(props.id)} className={InterviewerClass}>
+  <img
+    className="interviewers__item-image"
+    src={props.avatar}
+    alt={props.name}
+  />
+  {props.selected ? props.name : ""}
+</li>
+  )
+}
+
+export default InterviewerListItem;
