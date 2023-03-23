aa4ee41b4567b04bdc2d47eb775401cc0befb6b8
tylerchessa <tylerchessa@gmail.com>
2023-03-23 15:02:47 -0700

    fix time not showing in header

--- src/components/Application.js
+++ src/components/Application.js
@@ -46,8 +46,12 @@ const appointments = {
 
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday")
-  const [days, setDays] = useState([])
+  const [state, setState] = useState({
+    day: "Monday",
+    days: []
+  })
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
   useEffect(() => {
    axios.get("/api/days")
    .then((response) => {
@@ -55,7 +59,7 @@ export default function Application(props) {
     setDays(response.data)
    })
    .catch(err => console.log(err))
-  }, [day])
+  }, [state.day])
 
   return (
     <main className="layout">
@@ -67,8 +71,8 @@ export default function Application(props) {
 />
 <hr className="sidebar__separator sidebar--centered" />
 <nav className="sidebar__menu"><DayList
-  days={days}
-  value={day}
+  days={state.days}
+  value={state.day}
   onChange={setDay}
 /></nav>
 <img
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -9,10 +9,9 @@ import Empty from "./Empty";
 function Appointment(props) {
 
 const interview = props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />
-
   return (
     <article className="appointment">
-      <Header>{props.time}</Header>
+      <Header time={props.time} />
       {interview}
     </article>
   )
