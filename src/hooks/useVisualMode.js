aac0e1cb64fc45ef4e2e63be4798fbf88d6993e9
tylerchessa <tylerchessa@gmail.com>
2023-03-24 14:03:01 -0700

    implement useVisualMode

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { useState } from "react";
+
+function useVisualMode(initial) {
+  const [history, setHistory] = useState([initial])
+function transition(mode, boolean = false) {
+  setHistory(prev => boolean ? [...prev.slice(0, -1), mode] : [...prev, mode])
+}
+function back() {
+ setHistory(prev => (prev.length > 1 ? prev.slice(0, -1) : prev))
+}
+return { mode: history[history.length-1], transition, back };
+
+//   const [mode, setMode] = useState(initial)
+//   const [history, setHistory] = useState([initial])
+//   function transition(newMode, boolean = false) {
+//     if (boolean) {
+//       const newHistory = [...history]
+//     newHistory.pop() 
+//     setHistory([...newHistory, newMode])
+//   } else {setHistory([...history, newMode])}
+//     setMode(newMode)
+//   }
+//   function back() {
+//     if (history.length > 1) {
+//       const newHistory = [...history]
+//       newHistory.pop()
+//       const back1 = newHistory.length - 1 
+//     setMode(newHistory[back1])
+//     setHistory(newHistory)
+//   }
+// }
+//   return { mode, transition, back };
+}
+
+
+
+export default useVisualMode;
